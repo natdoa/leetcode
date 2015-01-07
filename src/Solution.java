831de6f05143d70c61353998f533a1a3187a5242
natdoa <bigben619@gmail.com>
2015-01-07 15:36:22 -0500

    lc-compareversionnumbers

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] a = version1.split("\\.");
+        String[] b = version2.split("\\.");
+        for(int i=0; i<a.length || i<b.length; ++i){
+            if(i<a.length && i<b.length){
+                if(Integer.valueOf(a[i]) > Integer.valueOf(b[i])) return 1;
+                if(Integer.valueOf(a[i]) < Integer.valueOf(b[i])) return -1;
+            }
+            else if(i<a.length){
+                if(Integer.valueOf(a[i]) > 0 ) return 1;
+            }
+            else{
+                if(Integer.valueOf(b[i]) > 0) return -1;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s=new Solution();
+		System.out.println(s.compareVersion("0.3", "1.1"));
+	}
+
+}
